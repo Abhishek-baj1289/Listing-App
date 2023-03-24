ae803b9bf43fc09ed964b0f1bfd68aacae79eed1
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-03-24 17:29:45 +0530

    completed

--- src/Content.jsx
+++ src/Content.jsx
@@ -23,5 +23,28 @@ const Content = ({ items, handleCheck, handleDelete }) => {
 export default Content
 
 /*
-Twinkle 
+Twinkle twinke little star
+how i wonder what you are 
+up above the world so high 
+lika a diamond in the sky
+
+when the blazing sun is gone
+when the nothing shines upon 
+then you show you little light 
+twinkle twinkle all the night
+
+then the traveller in the dark
+will thank you for your little spark
+for he could not see which way to go
+if you did not twinkle so
+
+in the dark blue sky you keep
+often through my curtains peep
+for you never shut your eye 
+till the sun is in the sky
+
+as your bright and tiny spark
+light the traveller in the dark
+though i not know what you are
+twinke twinkle little star
 */
